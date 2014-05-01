65d40e538d00791ab2699ceef0a44c9f623838d2
Josh Austin <Josh.Austin@gmail.com>
2014-05-01 06:58:24 -0400

    initial commit

---
+++ email_css_inliner/email_css_inliner.py
@@ -0,0 +1,40 @@
+__author__ = 'ntrepid8'
+from tornado.web import RequestHandler
+from premailer import Premailer
+import markdown
+import logging
+import os
+bootstrap_path = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        '..', 'css', 'bootstrap-simple.css'
+    )
+)
+
+
+class EmailCssInlinerRequestHandler(RequestHandler):
+
+    def get(self, *args, **kwargs):
+        self.write('post document to this URL to have CSS inlined')
+
+    def post(self, *args, **kwargs):
+        md_one = ''.join([
+            '<div class="content-wrapper">',
+            md_to_html(self.request.body.decode('utf-8')),
+            '</div>'
+        ])
+        logging.info(md_one)
+        html_text = inline_css(md_one)
+        self.write(html_text)
+
+
+def md_to_html(md_input):
+    return markdown.markdown(md_input)
+
+
+def inline_css(html_input):
+    return Premailer(
+        html_input,
+        external_styles=[
+            bootstrap_path
+        ]).transform()
---
+++ main.py
@@ -0,0 +1,22 @@
+__author__ = 'ntrepid8'
+from email_css_inliner.email_css_inliner import EmailCssInlinerRequestHandler
+import tornado.options
+import tornado.ioloop
+import tornado.web
+import logging
+logger = logging.getLogger(__name__)
+
+
+def main():
+    tornado.options.parse_command_line()
+    logger.info('md to html with inline css starting...')
+    application = tornado.web.Application(
+        [(r"/", EmailCssInlinerRequestHandler), ],
+        debug=True,
+        autoreload=True
+    )
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    main()
