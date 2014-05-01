3887605636815353bf18c29ad2fe2d2cee4d5509
Josh Austin <Josh.Austin@gmail.com>
2014-05-01 08:50:28 -0400

    added google analytics

--- email_css_inliner/email_css_inliner.py
+++ email_css_inliner/email_css_inliner.py
@@ -15,6 +15,18 @@ class EmailCssInlinerRequestHandler(RequestHandler):
     simple_form = """
     <!DOCTYPE html>
     <html>
+    <head>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-7507054-16', 'maasive.net');
+      ga('send', 'pageview');
+
+    </script>
+    </head>
     <body>
 
     <h1>MaaSive.net BootStrap Inliner</h1>
