928ffd1a241235face5e9e1bca0e2be72cabf906
Josh Austin <Josh.Austin@gmail.com>
2014-05-01 08:43:45 -0400

    added simple form to facilitate manual usage of the web-service

--- email_css_inliner/email_css_inliner.py
+++ email_css_inliner/email_css_inliner.py
@@ -2,7 +2,6 @@ __author__ = 'ntrepid8'
 from tornado.web import RequestHandler
 from premailer import Premailer
 import markdown
-import logging
 import os
 bootstrap_path = os.path.abspath(
     os.path.join(
@@ -13,14 +12,38 @@ bootstrap_path = os.path.abspath(
 
 
 class EmailCssInlinerRequestHandler(RequestHandler):
+    simple_form = """
+    <!DOCTYPE html>
+    <html>
+    <body>
+
+    <h1>MaaSive.net BootStrap Inliner</h1>
+
+    <p>Use the form or POST to this URL to get your Markdown document converted to HTML
+    and Bootstrap CSS inserted inline.  This is great for emails!</p>
+
+    <form name="md-input" action="/" method="post">
+        <div><textarea name="text" rows="12" cols="80" autofocus>Paste your markdown here!</textarea></div>
+        <div><input type="submit" value="Submit"></div>
+    </form>
+
+    <div>
+    <em>You can also POST directly to http://inliner.maasive.net/ with your markdown text
+    in the "text" field of the form to use the Bootstrap Inliner as a web-service.
+    </div>
+
+    </body>
+    </html>
+    """
 
     def get(self, *args, **kwargs):
-        self.write('post document to this URL to have CSS inlined')
+        self.write(self.simple_form)
 
     def post(self, *args, **kwargs):
+        md_raw = self.get_argument('text')
         md_one = ''.join([
             '<div class="content-wrapper">',
-            md_to_html(self.request.body.decode('utf-8')),
+            md_to_html(md_raw),
             '</div>'
         ])
         html_text = inline_css(md_one)
