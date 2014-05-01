ca6ea180e61fa05127b23bd494bccb6f66dba2c2
Josh Austin <Josh.Austin@gmail.com>
2014-05-01 09:04:31 -0400

    added mit license

--- email_css_inliner/email_css_inliner.py
+++ email_css_inliner/email_css_inliner.py
@@ -29,7 +29,7 @@ class EmailCssInlinerRequestHandler(RequestHandler):
     </head>
     <body>
 
-    <h1>MaaSive.net BootStrap Inliner</h1>
+    <h1><a href="https://maasive.net">MaaSive.net</a> <a href="http://getbootstrap.com/">BootStrap</a> Inliner</h1>
 
     <p>Use the form or POST to this URL to get your Markdown document converted to HTML
     and Bootstrap CSS inserted inline.  This is great for emails!</p>
@@ -40,8 +40,49 @@ class EmailCssInlinerRequestHandler(RequestHandler):
     </form>
 
     <div>
+    This webservice makes use of the following Python Modules:
+    </div>
+    <div>
+    <ul>
+        <li>
+            <strong>Tornado:</strong> <a href="https://pypi.python.org/pypi/tornado/3.2">https://pypi.python.org/pypi/tornado/3.2</a>
+        </li>
+        <li>
+            <strong>Premailer:</strong> <a href="https://pypi.python.org/pypi/premailer/2.1.0">https://pypi.python.org/pypi/premailer/2.1.0</a>
+        </li>
+        <li>
+            <strong>Markdown:</strong> <a href="https://pypi.python.org/pypi/Markdown">https://pypi.python.org/pypi/Markdown</a>
+        </li>
+    </ul>
+    </div>
+
+    <div><p>
     <em>You can also POST directly to http://inliner.maasive.net/ with your markdown text
     in the "text" field of the form to use the Bootstrap Inliner as a web-service.
+    </p></div>
+
+    <div>
+    <p>The MIT License (MIT)</p>
+
+    <p>Copyright (c) 2014 MaaSive.net LLC</p>
+
+    <p>Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:</p>
+
+    <p>The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.</p>
+
+    </p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.</p>
     </div>
 
     </body>
